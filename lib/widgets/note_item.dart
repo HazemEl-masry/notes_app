efca53e1c1b7f2bbd277cc5e6e371a9efed75899
Hazem Elmasry <hazemelmasryofficial@gmail.com>
2025-11-03 02:11:37 +0200

    finish note item

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -7,7 +7,7 @@ class NoteItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 16, 140, 189),
+        color: const Color.fromARGB(255, 156, 104, 25),
         borderRadius: BorderRadius.circular(18.0),
       ),
       padding: const EdgeInsets.all(16.0),
@@ -15,25 +15,37 @@ class NoteItem extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           ListTile(
-            title: const Text("Flutter Tips"),
-            titleTextStyle: const TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+            title: const Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: Text(
+                maxLines: 1,
+                "Flutter Tips",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-            subtitle: const Text("Stay focus in your way"),
-            subtitleTextStyle: const TextStyle(fontWeight: FontWeight.bold),
+            subtitle: const Text(
+              maxLines: 2,
+              "Stay focus in your way",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             trailing: IconButton(
               iconSize: 40,
               onPressed: () {},
               icon: const Icon(Icons.delete),
-              color: Colors.red,
+              color: Colors.white54,
             ),
           ),
           const SizedBox(height: 20.0),
-          const Text('2025/11/02',style: TextStyle(
-            fontWeight: FontWeight.w500,
-            color: Colors.white
-           ),
+          const Text(
+            'Nov 3,2025',
+            style: TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
           ),
         ],
       ),
