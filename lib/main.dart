4412103c042557eedace9cb2a169472b48dfb6e6
Hazem Elmasry <hazemelmasryofficial@gmail.com>
2025-11-05 01:58:59 +0200

    init hive and notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/screens/notes_screen.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.openBox("notes_box");
   runApp(const MyApp());
 }
 
