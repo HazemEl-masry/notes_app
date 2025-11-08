3f4dd325361f6c22e6a94f39fe4507dc5e2f54a9
Hazem Elmasry <hazemelmasryofficial@gmail.com>
2025-11-08 03:58:41 +0200

    refactor

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:meta/meta.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
@@ -10,7 +10,11 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+  Color color = Colors.blue;
+
   addNote(NoteModel note) async {
+    // ignore: deprecated_member_use
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>("notes_box");
--- lib/widgets/colors_list_widget.dart
+++ lib/widgets/colors_list_widget.dart
@@ -13,9 +13,9 @@ class _ColorsListWidgetState extends State<ColorsListWidget> {
   List<Color> colors = [
     const Color(0xffAC3931),
     const Color(0xffE5D352),
-    Color(0xffD9E76C),
-    Color(0xff537D8D),
-    Color(0xff482C3D),
+    const Color(0xffD9E76C),
+    const Color(0xff537D8D),
+    const Color(0xff482C3D),
   ];
   @override
   Widget build(BuildContext context) {
